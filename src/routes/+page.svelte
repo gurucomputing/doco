fed6cbe3427a6f4694e74865aba0eef1c768302d
Chris Bisset <chris@gurucomputing.com.au>
2023-12-20 20:35:55 +1100

    misc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,11 +24,6 @@
 					<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 				</svg> <input type="text" placeholder="search" class="input input-bordered input-xs w-full max-w-xs" />
 			</div>
-			<div class="flex flex-none">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-3 mr-1 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-					<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-				</svg> <input type="text" placeholder="search" class="input input-bordered input-xs w-full max-w-xs" />
-			</div>
 			<li><a href=".">Sidebar Item 1</a></li>
 			<li><a href=".">Sidebar Item 2</a></li>
 		</ul>
